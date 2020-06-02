ae84a96c4f9765459d92764da895911bed294e90
Patrice Weber <patrice.weber@cdu.edu.au>
2020-06-02 16:51:37 +0930

    First deployed package version 1.1

--- build/lib/sentinel/__init__.py
+++
@@ -1,9 +0,0 @@
-
-""" Sentinel module:
-    Contains all the plumbing (functions and classes) necessary to make the workflow
-    script run smooothly """
-
-import sentinel.utils
-import sentinel.downloader
-import sentinel.workflow
-
